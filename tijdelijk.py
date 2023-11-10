53d96d675b641f83382962d91f6cb26699298959
Tim de Jonge <tim-94@hotmail.com>
2023-11-10 21:23:42 +0100

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,21 +1,27 @@
-prijzen = {
-    "aardbei" : 3,
-    "vanille" : 4,
-    "chocolade" : 5
-}
-aanbieding = prijzen["aardbei"] * 0.8
-reclame_tekst = f"Vandaag in de aanbieding: aardbeien-ijs, 1 liter - slechts €{aanbieding}"
+from helper import decoreer
 
-reclame_tekst2 = reclame_tekst[:64]
-#Ik heb de index van de tweede 0 achter de komma gepakt, omdat bedragen altijd twee cijfers achter de komma hebben.
-#mocht dit niet goed gerekend worden dan moet er[:63] staan!
+def print_aanbieding():
+    prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5
+    }
 
-reclame_tekst3 = reclame_tekst2.upper()
+    aanbieding = prijzen["aardbei"] * 0.8
 
-reclame_tekst4 = reclame_tekst3.split()
+    reclame_tekst = f"Vandaag in de aanbieding: aardbeien-ijs, 1 liter - slechts €{aanbieding}"
 
-for el in reclame_tekst4:
-    if len(el) <5:
-        print(el.lower()) 
-    else:
-        print(el.upper())
+    reclame_tekst2 = reclame_tekst[:64]
+    
+    reclame_tekst3 = reclame_tekst2.upper()
+
+    reclame_tekst4 = reclame_tekst3.split()
+
+    for el in reclame_tekst4:
+        if len(el) <5:
+            print(el.lower()) 
+        else:
+            print(el.upper())
+
+decoreer("Aanbieding")
+print_aanbieding()
